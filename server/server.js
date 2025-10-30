f46fd5c7ba468d33b4500b54086867cb1c3cf69a
Saurabh maurya <saurabh.maurya120703@gmail.com>
2025-10-30 19:39:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,9 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://e-commerce-web-client-0asd.onrender.com", 
+      "https://e-commerce-web-git-main-saurabh-mauryas-projects-a4bfe440.vercel.app",
+
+     // "https://e-commerce-web-client-0asd.onrender.com", 
     ],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
